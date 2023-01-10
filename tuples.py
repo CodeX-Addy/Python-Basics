229d9549c80a21881797bdf7ae1d40066acfcdac
Aditya Tomar <98268205+CodeX-Addy@users.noreply.github.com>
2023-01-10 19:24:54 +0530

    Create tuples.py

---
+++ tuples.py
@@ -0,0 +1,9 @@
+# A tuple is an immutable (can’t change or modified) data type in Python.
+a = ()              #It is an example of empty tuple
+a = (1,)           #Tuple with only one element needs a comma
+a = (1, 7, 2)   #Tuple with more than one element
+
+# Tuple methods
+a = (1, 7, 2)
+count(1) – It will return the number of times 1 occurs in a.
+index(1) – It will return the index of the first occurrence of 1 in a.
