ef850a9359b8cb7f6b9c08b62172d59cb189c09a
Aditya Tomar <98268205+CodeX-Addy@users.noreply.github.com>
2023-01-12 21:00:16 +0530

    Create conditionalExpressions.py

---
+++ conditionalExpressions.py
@@ -0,0 +1,38 @@
+'''
+if (condition1):		// if condition 1 is true
+	print(“yes”)
+elif (condition2):		// if condition 2 is true
+	print(“No”)
+else:				// otherwise
+	print(“May be”)
+'''
+a = 22
+if (a>9):
+    print(“Greater”)
+else:
+    print(“lesser”)
+     
+#Relational operators
+= = -> equals
+
+>=  -> greater than/equal to
+
+<=, etc.
+
+#Logical operators
+and -> true if both operands are true else false
+
+or -> true if at least one operand is true else false
+
+not -> inverts true to false and false to true
+
+'''
+if (condition1):
+    #code
+elif (condition 2): //elif clause
+    #code
+elif (condition 2):
+    #code
+….
+else:
+    #code    '''
