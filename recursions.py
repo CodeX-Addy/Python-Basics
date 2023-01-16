e922ef77a98d1db494dd9554f1917aa4e782164f
Aditya Tomar <98268205+CodeX-Addy@users.noreply.github.com>
2023-01-16 20:17:12 +0530

    Create recursions.py

---
+++ recursions.py
@@ -0,0 +1,7 @@
+factorial(n) = n * factorial(n-1)
+def factorial(n):
+	if i == 0 or i == 1 : #Base condition which doesn’t call the function any further
+		return i
+	else:
+		return n*factorial(n-1) #Function calling itself
+
