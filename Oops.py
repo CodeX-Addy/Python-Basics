2e484b08aa8abd4707360dc99847ecb3072394fe
Aditya Tomar <98268205+CodeX-Addy@users.noreply.github.com>
2023-01-19 20:29:08 +0530

    Create Oops.py

---
+++ Oops.py
@@ -0,0 +1,38 @@
+Class Employee:		[classname is written in PascalCase]
+	#methods & variables
+
+#Class Attributes  
+Class Employee:
+	company = “Google” 	#Specific to each class
+Aditya = Employee()	#Object instantiation
+Aditya.company
+Employee.company = “Github”	#changing class attribute
+
+#Instance Attributes
+print(“Hello World”)         # print is a function (more later)
+
+harry.getSalary()
+
+class Employee:
+	company = “Google”
+	def getSalary(self):
+		print(“Salary is not there”)
+
+#Static method
+@staticmethod	#decorator to mark greet as a static method
+def greet():
+	print(“Hello user”)
+
+#__init__() constructor
+class Employee:
+	def __init__(self,name):
+		self.name = name
+	def getSalary(self):
+		#Some code…
+harry = Employee(“Harry”)	#Object can be instantiated using constructor like this!
+
+
+
+
+
+
