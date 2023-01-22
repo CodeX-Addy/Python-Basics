6a6e87c61ecb201137a26b49624bff89c8976c5d
Aditya Tomar <98268205+CodeX-Addy@users.noreply.github.com>
2023-01-22 13:36:38 +0530

    Create Inheritance.py

---
+++ Inheritance.py
@@ -0,0 +1,32 @@
+#Syntax:
+class Employee:	#Base Class
+	#Code
+class Programmer(Employee):	#Derived or child class
+	#Code
+  
+# Super() method
+super().__init__()  #Calls constructor of the base class
+
+# Class methods
+@classmethod
+def (cls, p1, p2):
+	#code
+# @property decorators
+class Employee:
+	@property 
+	def name(self):
+		return self.ename
+
+#   @.getters and @.setters
+@name.setter
+def name(self, value):
+	self.ename = value
+
+
+
+
+
+
+
+
+
