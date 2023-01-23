8c7064b493e92afb3e05bc05d41807b928723b91
Aditya Tomar <98268205+CodeX-Addy@users.noreply.github.com>
2023-01-23 21:03:14 +0530

    Update Inheritance.py

--- Inheritance.py
+++ Inheritance.py
@@ -22,6 +22,23 @@ class Employee:
 def name(self, value):
 	self.ename = value
 
+	
+#Operator overloading in Python
+p1 + p2 -> p1.__add__(p2)
+
+p1 – p2 -> p1.__sub__(p2)
+
+p1 * p2 -> p1.__mul__(p2)
+
+p1 / p2 -> p1.__truediv__(p2)
+
+p1 // p2 -> p1.__floordiv__(p2)
+
+#Other dunder/magic methods in Python
+__str__() -> used  to set what gets displayed upon calling str(obj)
+
+__len__() -> used to set what gets displayed upon calling .__len__() or len(obj)
+
 
 
 
