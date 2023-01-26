469f33b7734703c6cf4803a1ded7e1193a5d0de0
Aditya Tomar <98268205+CodeX-Addy@users.noreply.github.com>
2023-01-26 22:59:56 +0530

    Create pythonScopes.py

---
+++ pythonScopes.py
@@ -0,0 +1,37 @@
+#Local Scope
+# A variable created inside a function is available inside that function
+def myfunc():
+  x = 300
+  print(x)
+
+myfunc()
+
+#Function inside function
+# The local variable can be accessed from a function within the function
+def myfunc():
+  x = 300
+  def myinnerfunc():
+    print(x)
+  myinnerfunc()
+
+myfunc()
+
+#Global Scope
+#A variable created outside of a function is global and can be used by anyone:
+x = 300
+def myfunc():
+  print(x)
+myfunc()
+print(x)
+
+#Naming variables
+# The function will print the local x, and then the code will print the global x:
+x = 300
+
+def myfunc():
+  x = 200
+  print(x)
+
+myfunc()
+
+print(x)
