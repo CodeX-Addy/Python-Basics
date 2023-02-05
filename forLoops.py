41d0370a142559777e7c06379423c133ce124487
Aditya Tomar <98268205+CodeX-Addy@users.noreply.github.com>
2023-02-05 12:19:59 +0530

    Create forLoops.py

---
+++ forLoops.py
@@ -0,0 +1,40 @@
+for val in sequence:
+    # statement(s)
+    
+languages = ['Swift', 'Python', 'Go', 'JavaScript']
+
+# access items of a list using for loop
+for language in languages:
+    print(language)
+# Output
+# Swift
+# Python
+# Go
+# JavaScript
+
+#By using range function
+# use of range() to define a range of values
+values = range(4)
+
+# iterate from i = 0 to i = 3
+for i in values:
+    print(i)
+# Output
+# 0
+# 1
+# 2
+# 3
+
+# For loop with else
+digits = [0, 1, 5]
+
+for i in digits:
+    print(i)
+else:
+    print("No items left.")
+# Output
+# 0
+# 1
+# 5
+# No items left.
+ 
