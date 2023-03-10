b9ed0a62eef6f535f1f6885091be6fdbc162134b
Aditya Tomar <98268205+CodeX-Addy@users.noreply.github.com>
2023-03-10 10:19:47 +0530

    Update searching.py

--- searching.py
+++ searching.py
@@ -7,3 +7,40 @@ def search(arr, x):
 			return i
 
 	return -1
+
+# BINARY SEARCH
+def binary_search(arr, x):
+	low = 0
+	high = len(arr) - 1
+	mid = 0
+
+	while low <= high:
+
+		mid = (high + low) // 2
+
+		# If x is greater, ignore left half
+		if arr[mid] < x:
+			low = mid + 1
+
+		# If x is smaller, ignore right half
+		elif arr[mid] > x:
+			high = mid - 1
+
+		# means x is present at mid
+		else:
+			return mid
+
+	# If we reach here, then the element was not present
+	return -1
+
+
+# Given array
+arr = [ 2, 3, 4, 10, 40 ]
+x = 10
+
+result = binary_search(arr, x)
+
+if result != -1:
+	print("Element is present at index", str(result))
+else:
+	print("Element is not present in array")
