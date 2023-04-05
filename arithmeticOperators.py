ca2423ca28b2825a24f3dcc211c7114c9c6337d9
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2023-04-05 20:04:31 +0530

    Create arithmeticOperators.py

---
+++ arithmeticOperators.py
@@ -0,0 +1,10 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+a = 5
+b = 6
+print(a/b)#It will divide a and b
+print(a//b)#It will return the integer value
+print(a*b)
+print(a+b)
+print(a-b)
+print(a**b)#It will give the a exponent to b
