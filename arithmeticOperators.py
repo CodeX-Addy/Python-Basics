eaa05fee99cc7c7fc7a8e8dce22836c1fdb94345
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2023-04-05 20:05:35 +0530

    Update arithmeticOperators.py

--- arithmeticOperators.py
+++ arithmeticOperators.py
@@ -1,8 +1,8 @@
 a = 5
-b = 6#Assigning the integer value to the variables
-print(a/b)#It will divide a and b
-print(a//b)#It will return the integer value
+b = 6 #Assigning the integer value to the variables
+print(a/b) #It will divide a and b
+print(a//b) #It will return the integer value
 print(a*b)
 print(a+b)
 print(a-b)
-print(a**b)#It will give the a exponent to b
+print(a**b) #It will give the a exponent to b
