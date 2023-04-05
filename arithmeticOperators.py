79418286d9c2efec45098bc33f4e66a19beb40f6
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2023-04-05 20:05:16 +0530

    Update arithmeticOperators.py

--- arithmeticOperators.py
+++ arithmeticOperators.py
@@ -1,7 +1,5 @@
-# Online Python compiler (interpreter) to run Python online.
-# Write Python 3 code in this online editor and run it.
 a = 5
-b = 6
+b = 6#Assigning the integer value to the variables
 print(a/b)#It will divide a and b
 print(a//b)#It will return the integer value
 print(a*b)
