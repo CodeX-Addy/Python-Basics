12d23fc984f6c7c08714f0adb73a5d9ecae158b9
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2023-05-07 00:12:03 +0530

    Update arithmeticOperators.py

--- arithmeticOperators.py
+++ arithmeticOperators.py
@@ -4,5 +4,5 @@ print(a/b) #It will divide a and b
 print(a//b) #It will return the integer value
 print(a*b)#Multiplication operator
 print(a+b)#Addition operator
-print(a-b)
+print(a-b)#Subtraction operator
 print(a**b) #It will give the value of a exponent to b
