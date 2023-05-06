776757399ab94972bed0be56df4c7d825611087a
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2023-05-07 00:11:44 +0530

    Update arithmeticOperators.py

--- arithmeticOperators.py
+++ arithmeticOperators.py
@@ -3,6 +3,6 @@ b = 6 #Assigning the integer value to the variables
 print(a/b) #It will divide a and b
 print(a//b) #It will return the integer value
 print(a*b)#Multiplication operator
-print(a+b)
+print(a+b)#Addition operator
 print(a-b)
 print(a**b) #It will give the value of a exponent to b
