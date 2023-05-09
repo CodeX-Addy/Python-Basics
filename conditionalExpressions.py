227ac3598a4818dce792c905aa249a6c46d70a43
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2023-05-09 15:32:17 +0530

    Update conditionalExpressions.py

--- conditionalExpressions.py
+++ conditionalExpressions.py
@@ -1,11 +1,4 @@
-'''
-if (condition1):		// if condition 1 is true
-	print(“Yes”)
-elif (condition2):		// if condition 2 is true
-	print(“No”)
-else:				// otherwise
-	print(“May be”)
-'''
+
 a = 22 #Indentation is important
 if (a>9):
     print(“Greater”)
