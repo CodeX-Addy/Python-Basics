a3ceb78927b9abb84f63a308bb51858faf99f914
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2023-05-20 16:14:31 +0530

    Update exceptions.py

--- exceptions.py
+++ exceptions.py
@@ -1,6 +1,12 @@
-
 try:
     a = int(input("Enter your number:"))
     print(a + 1)
 except:
     print("Some error occured!!")
+    
+try:
+    a = int(input("Enter your number:"))
+    print(a + 1)
+except Exception as ex:
+    print("Some error occured due to",ex)#It'll give the reason for error
+
