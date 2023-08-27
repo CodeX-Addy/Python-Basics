9a3ce4abbf42920175add6682eec1dffb1356e81
Aditya Tomar <aditya.tomar9055049@gmail.com>
2023-08-27 19:38:05 +0530

    exceptions

--- exceptions.py
+++ exceptions.py
@@ -8,5 +8,6 @@ try:
     a = int(input("Enter your number:"))
     print(a + 1)
 except Exception as ex:
-    print("Some error occured due to",ex)#It'll give the reason for error
+    print("Some error occured due to",ex)
+    #It'll give the reason for error
 
