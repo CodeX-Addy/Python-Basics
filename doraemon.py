aa071e16d6bbd21c3c490a3141b544c06c865c19
Aditya Tomar <98268205+CodeX-Addy@users.noreply.github.com>
2024-10-07 12:19:23 +0530

    .

--- doraemon.py
+++ doraemon.py
@@ -300,5 +300,5 @@ if __name__ == '__main__':
     speed(9)
     Doraemon()
     my_goto(100, -300)
-    write('@xdcoder.xyz', font=("Bradley Hand ITC", 30, "bold",))
+    write('@eso', font=("Bradley Hand ITC", 30, "bold",))
     mainloop()
