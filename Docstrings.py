47903b301bfa6ecbfeb77ec9ee9490a9f11383ea
Aditya Tomar <98268205+CodeX-Addy@users.noreply.github.com>
2024-11-08 14:54:28 +0530

    docstrings in python

---
+++ Docstrings.py
@@ -0,0 +1,14 @@
+'''
+Docstrings, short for documentation strings, are vital in conveying the purpose and functionality of python functions, modules, and classes.
+'''
+def my_function():
+    '''Demonstrates triple double quotes
+    docstrings and does nothing really.'''
+ 
+    return None
+
+print("Using __doc__:")
+print(my_function.__doc__)
+
+print("Using help:")
+help(my_function)
