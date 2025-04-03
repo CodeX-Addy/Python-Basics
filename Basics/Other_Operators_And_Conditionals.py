35f84f761a73a9fd53d412387304384ca1a98437
CodeX-Addy <aditya.tomar9055049@gmail.com>
2025-04-03 13:51:10 +0530

    add: conditionals

---
+++ Basics/Other_Operators_And_Conditionals.py
@@ -0,0 +1,26 @@
+a = 22 
+if (a>9):
+    print("Greater")
+else:
+    print("Lesser")
+     
+## Relational Operators
+'''
+== -> equals
+>=  -> greater than/equal to
+<= etc.
+'''
+
+## Logical Operators
+'''
+and -> true if both operands are true else false
+or -> true if at least one operand is true else false
+not -> inverts true to false and false to true
+'''
+
+## Bitwise Operators
+'''
+& -> Bitwise and
+| -> Bitwise or
+~ -> Bitwise not/tilde operator
+'''
