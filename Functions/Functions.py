feabb9f970f9d93efd2b00c8325bcdd6266caf5e
CodeX-Addy <aditya.tomar9055049@gmail.com>
2025-04-04 10:32:00 +0530

    add: functions

--- Functions/Functions.py
+++ Functions/Functions.py
@@ -1,21 +1,20 @@
 Ex-
 def func1():
-	print(“Hello”)
+	print("Hello")
   
 func1()          #This is called function call
 
 #Functions with arguments
 def greet(name):
-	gr = “Hello” + name
+	gr = "Hello" + name
 	return gr
-        a = greet(“python”) #“python” is passed to greet in name
+        a = greet("python") #“python” is passed to greet in name
 
 # a will now contain “Hello python”
 
 # Default Parameter Value
-def greet(name=’stranger’):
+def greet(name='stranger'):
 	#function body
 greet()                       #Name will be ‘stranger’ in function body(default)
-
-greet(“Aditya”)        #Name will be “Aditya” in function body(passed)
+greet("Aditya")        #Name will be “Aditya” in function body(passed)
 
--- Functions/Recursion.py
+++ Functions/Recursion.py
@@ -1,3 +1,5 @@
+## Simple factorial program to demonstrate the recusrsion's working
+
 factorial(n) = n * factorial(n-1)
 def factorial(n):
 	if i == 0 or i == 1 : #Base condition which doesn’t call the function any further
