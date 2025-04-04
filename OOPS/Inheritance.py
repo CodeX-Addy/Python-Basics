403f5d966bfadef5a3dd803b18fe7a5546de6993
CodeX-Addy <aditya.tomar9055049@gmail.com>
2025-04-04 10:19:31 +0530

    add: oops

--- OOPS/Inheritance.py
+++ OOPS/Inheritance.py
@@ -1,4 +1,4 @@
-#Syntax:
+## Syntax:
 class Employee:	#Base Class
 	#Code
 class Programmer(Employee):	#Derived or child class
@@ -11,35 +11,37 @@ super().__init__()  #Calls constructor of the base class
 @classmethod
 def (cls, p1, p2):
 	#code
+
 # @property decorators
 class Employee:
 	@property 
 	def name(self):
 		return self.ename
 
-#   @.getters and @.setters
+# @.getters and @.setters
 @name.setter
 def name(self, value):
 	self.ename = value
 
-	
 #Operator overloading in Python
+'''
 p1 + p2 -> p1.__add__(p2)
 
-p1 – p2 -> p1.__sub__(p2)
+p1 - p2 -> p1.__sub__(p2)
 
 p1 * p2 -> p1.__mul__(p2)
 
 p1 / p2 -> p1.__truediv__(p2)
 
 p1 // p2 -> p1.__floordiv__(p2)
+'''
 
-#Other dunder/magic methods in Python
+## Other dunder/magic methods in Python
+'''
 __str__() -> used  to set what gets displayed upon calling str(obj)
 
 __len__() -> used to set what gets displayed upon calling .__len__() or len(obj)
-
-
+'''
 
 
 
---
+++ OOPS/Intro.py
@@ -0,0 +1,35 @@
+Class Employee:	     ## [classname is written in PascalCase]
+	## Methods & Variables
+
+## Class Attributes  
+Class Employee:
+	company = "XYZ" 	## Specific to each class
+Aditya = Employee()	## Object instantiation
+Aditya.company
+Employee.company = "Github" ## Changing class attribute
+
+## Instance Attributes
+print("Hello World") 
+
+class Employee:
+	company = "Google"
+	def getSalary(self):
+		print("Salary is not there")
+
+## Static method
+@staticmethod	
+def greet():
+	print(“Hello user”)
+
+## __init__() constructor
+class Employee:
+	def __init__(self,name):
+		self.name = name
+	def getSalary(self):
+X = Employee("Aditya")## Object can be instantiated using constructor like this!
+
+
+
+
+
+
