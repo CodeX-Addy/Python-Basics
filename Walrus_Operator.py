903ec204e788d9966a12e0019cbf99763ed9a4ae
Aditya Tomar <98268205+CodeX-Addy@users.noreply.github.com>
2026-07-24 14:50:17 +0530

    add: walrus operator

--- Walrus_Operator.py
+++ Walrus_Operator.py
@@ -13,3 +13,11 @@ if (value := input("Enter your value: ")) in allowed_values:
     print(f"This {value} is present inside allowed values")
 else:
     print(f"Sorry, this {value} is not present inside the allowed values")
+
+## example 3
+
+allowed_values = ["a", "b", "c", "d"]
+
+while (value := input("Enter your value: ")) not in allowed_values:
+    print(f"The value you've entered is: {value}. Please check the allowed values")
+print("Okay so you've entered right value")
