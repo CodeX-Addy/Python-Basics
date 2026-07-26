06af08e4373c557096379b531bf6892fce9c9fdc
Aditya Tomar <98268205+CodeX-Addy@users.noreply.github.com>
2026-07-26 17:49:36 +0530

    add: list comprehensions

--- Comprehensions/list_comp.py
+++ Comprehensions/list_comp.py
@@ -3,3 +3,5 @@ greet_list = ["Good Morning", "Hello", "Good Evening", "Wish you the best"]
 hello_list = [hello for hello in greet_list if "Good" in hello]
 
 print(hello_list)
+
+## Output: ['Good Morning', 'Good Evening']
