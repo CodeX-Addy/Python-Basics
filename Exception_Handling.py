aa914e28d368c127b95acba9b88a7d9331613d96
Aditya Tomar <98268205+CodeX-Addy@users.noreply.github.com>
2026-08-09 13:01:16 +0530

    add: exception handling

--- Exception_Handling.py
+++ Exception_Handling.py
@@ -10,3 +10,17 @@ else:
     print(f"We're happy to server you {dictionary["size"]} ml coffee..")
     
     
+## handling multiple exceptions
+
+def print_order(item, quantity):
+    try:
+        price = {"latte": 70}[item]
+        cost = price * quantity
+        print(f"Your total cost is: {cost}")
+    except KeyError:
+        print("This type of coffee doesn't exist in our menu..")
+    except TypeError:
+        print("Integer value is required..")
+        
+print_order("mocha", 20)
+ 
