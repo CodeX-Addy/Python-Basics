5ae77501fce66b3feca256f401ceec25560379db
Aditya Tomar <98268205+CodeX-Addy@users.noreply.github.com>
2026-08-10 08:11:37 +0530

    add: multithreading in python

---
+++ Concurrency and Parallelism/multi_threading.py
@@ -0,0 +1,23 @@
+import threading
+import time
+
+def take_order():
+    for i in range(1, 4):
+        print(f"Taking order for customer {i}")
+        time.sleep(2)
+        
+def brew_coffee():
+    for i in range(1, 4):
+        print(f"Brewing coffee for customer {i}")
+        time.sleep(3)
+        
+order_thread = threading.Thread(target=take_order)
+brew_thread = threading.Thread(target=brew_coffee)
+
+order_thread.start()  ## starting the threads
+brew_thread.start()
+
+order_thread.join()
+brew_thread.join()
+
+print("Order is taken for brewing the coffee..")
