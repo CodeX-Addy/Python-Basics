fede3638d606f44eea9c850161b21ca9a969e193
Aditya Tomar <98268205+CodeX-Addy@users.noreply.github.com>
2026-08-16 10:10:17 +0530

    add: process queue

--- Concurrency and Parallelism/process_queue.py
+++ Concurrency and Parallelism/process_queue.py
@@ -3,9 +3,10 @@ from multiprocessing import Process, Queue
 def queue_working(queue):
     queue.put("This is a test..")
 
-queue = Queue()
-p = Process(target=queue_working, args=(queue,))
-p.start()
-p.join()
+if __name__ == "__main__":
+    queue = Queue()
+    p = Process(target=queue_working, args=(queue,))
+    p.start()
+    p.join()
 
-print(queue.get())
+    print(queue.get())
